f610b9258aa3687b5b74e89fb9d9fe972886b8f2
TZConsultingcorp <tmarquez@supraholding.net>
2026-07-17 09:05:21 +0800

    Update enroll.js

--- api/enroll.js
+++ api/enroll.js
@@ -1,6 +1,8 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    status: "OK",
-    message: "Kribatta API funcionando correctamente"
-  });
-}
+
+Parameter	Description	Example
+ccode	Your API access key provided by Kribatta
+Do not expose this key to the players	
+name	Player name. Optional	William Mills
+account	Player Account or Code in your system (This is the identifier that will show on reports and sent in callbacks)	STO8311
+session_key	An unique session key	d98a6fa00744a50a8dd869d8223fd877
+agent	OPTIONAL. Agent code if related	AF7719
