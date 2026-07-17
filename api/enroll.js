15a6a2064909df42a26910b8855410ada57452c4
TZConsultingcorp <tmarquez@supraholding.net>
2026-07-17 09:07:00 +0800

    Remove exposed API key from enroll.js
    Removed exposed API key from the documentation.

--- api/enroll.js
+++ api/enroll.js
@@ -1,6 +1,6 @@
 
 Parameter	Description	Example
-ccode	Your API access key provided by Kribatta
+ccode	Your API access key provided by Kribatta Remove exposed API key
 Do not expose this key to the players	
 name	Player name. Optional	William Mills
 account	Player Account or Code in your system (This is the identifier that will show on reports and sent in callbacks)	STO8311
